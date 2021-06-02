b8f7ebbb85f93050cff74feaafd3e16ef7631597
Arnold DuFauchard <arnold.dufauchard@gmail.com>
2021-06-02 12:56:22 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '306126944425631',
+        clientSecret: '5451a9f34a3ddf2bbf682175ceab251f'
+    }
 }
